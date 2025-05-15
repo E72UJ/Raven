539fd286d38b9429fc9d0134a901ebd6c7f6ff38
Furau <3873153379@qq.com>
2025-05-15 08:33:32 +0800

    更新颜色表格
    更新颜色表格

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use std::fs;
 use std::path::PathBuf;
 pub const FPS_OVERLAY_Z_INDEX: i32 = i32::MAX - 32;
 
+
+// 按钮组颜色表格
+
+const NORMAL_BUTTON: Color = Color::srgb(0.75, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+// 按钮组颜色表格结束
 // 主配置结构体
 #[derive(Debug, Deserialize, Resource)]
 struct MainConfig {
