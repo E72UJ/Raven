12d3838d37aa29d3899fff942951a5e133021b86
Furau <3873153379@qq.com>
2025-11-03 14:27:09 +0800

    修复干净的案例
    修正一个干净的案例

--- examples/fule.rs
+++ examples/fule.rs
@@ -1,10 +1,6 @@
-// examples/test_raven.rs
-use Raven::{raven::*, Rvn};
-// 或者
-// use your_engine::raven_prelude::*;  // 如果你只想使用 raven 相关功能
+use Raven::raven::prelude::*;
 
 fn main() {
-    // 调用你的函数
     let test = Rvn! {
         character Alice {
             name = "你好";
